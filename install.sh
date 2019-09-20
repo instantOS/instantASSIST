9386361535eb7b197fd89ed55496651fd75e5f96
paperbenni <paperbenni@gmail.com>
2019-09-20 12:10:57 +0200

    Update install.sh
    spotify adblock

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@ mkdir paperbenni
 cd paperbenni
 mkdir screenshots
 mkdir recordings music
+git clone --depth=1 "https://github.com/abba23/spotify-adblock-linux.git"
+cd spotify-adblock-linux
+sudo make iństall
+cd ..
 git clone --depth=1 "https://github.com/paperbenni/menus.git"
 cd menus
 sudo mv paperapps /usr/bin
