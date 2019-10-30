0fdce4336e31e8f91a889e500634a072986ef3c2
paperbenni <paperbenni@gmail.com>
2019-10-30 17:57:17 +0100

    fix wallpaper typo

--- dm/w.sh
+++ dm/w.sh
@@ -2,4 +2,4 @@
 
 mkdir ~/paperbenni &>/dev/null
 wget -O ~/paperbenni/wallpaper.png 'https://source.unsplash.com/random/1920x1080'
-feh --bg-scale ~/paperbenni/wallpaper.jpg
+feh --bg-scale ~/paperbenni/wallpaper.png
