48988168f95006b4186d45515494b5ca86b4eac1
paperbenni <paperbenni@gmail.com>
2019-10-30 18:02:17 +0100

    dynamic jpg

--- dm/w.sh
+++ dm/w.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 mkdir ~/paperbenni &>/dev/null
-wget -O ~/paperbenni/wallpaper.png 'https://source.unsplash.com/random/1920x1080'
-feh --bg-scale ~/paperbenni/wallpaper.png
+
+finalurl=$(curl 'https://source.unsplash.com/random/1920x1080' -s -L -I -o /dev/null -w '%{url_effective}')
+echo "final url = $finalurl"
+
+if echo "$finalurl" | grep 'jpg'; then
+    wget -O ~/paperbenni/wallpaper.jpg "$finalurl"
+    feh --bg-scale ~/paperbenni/wallpaper.jpg
+else
+    wget -O ~/paperbenni/wallpaper.png "$finalurl"
+    feh --bg-scale ~/paperbenni/wallpaper.png
+fi
