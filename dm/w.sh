c3be50d68a47715b466cf5dc9d90d67d16dfaa91
terminalforlife <terminalforlife@yahoo.com>
2019-11-11 19:19:37 +0000

    Remove need for file, by piping image to feh
    This may not be preferred, should you may wish to copy the wallpaper
    somewhere safely. Otherwise, this should be nice, as it won't take up
    space, just (presumably) memory.

--- dm/w.sh
+++ dm/w.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-file="$HOME/paperbenni/wallpaper"
 url='https://source.unsplash.com/random/1920x1080'
 finalurl=$(curl "$url" -s -L -I -o /dev/null -w '%{url_effective}')
 echo "final url = $finalurl"
 
 dl_and_set(){
-    if curl -s --create-dirs -o "$file.$1" "$finalurl"; then
-        feh --bg-scale "$file.$1"
-    fi
+    curl -s --create-dirs -o - "$finalurl" | feh --bg-scale -
 }
 
 case "$finalurl" in
