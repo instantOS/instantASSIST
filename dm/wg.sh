19220eda4e4b51bb96406156660e98fc3fae7bab
paperbenni <paperbenni@gmail.com>
2019-11-11 16:31:36 +0100

    wg

---
+++ dm/wg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#######################################
+## chromecast like wallpaper changer ##
+#######################################
+
+randomwallpaper() {
+    WALLPAPERNAME="$(curl 'https://storage.googleapis.com/chromeos-wallpaper-public/' |
+        egrep -o '<Key>[^<>]*</Key>' | grep 'resolution.*\.jpg' |
+        grep -o '>.*<' | grep -o '[^<>]*' |
+        shuf | head -1)"
+    grep -q 'resolution' <<<$WALLPAPERNAME || return 1
+    wget -O ~/paperbenni/wallpaper.jpg "https://storage.googleapis.com/chromeos-wallpaper-public/$WALLPAPERNAME"
+    feh --bg-scale ~/paperbenni/wallpaper.jpg
+
+}
