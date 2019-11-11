2796a52b30fe533010dff9ea2a4641fdc3197f9f
terminalforlife <terminalforlife@yahoo.com>
2019-11-11 19:29:07 +0000

    Simplify `w.sh` for an easy one-liner
    It occurred to me that this could be watered down to just the one line.
    This would make for a nice alias, or something to have executed upon
    logging into the user's account.
    
    Ignore the previous commit's error message, in which I stated only
    memory would be taken up, as, at least for me, a temporary file was
    created in `/tmp`, which is still good, I think.

--- dm/w.sh
+++ dm/w.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-url='https://source.unsplash.com/random/1920x1080'
-finalurl=$(curl "$url" -s -L -I -o /dev/null -w '%{url_effective}')
-echo "final url = $finalurl"
-
-dl_and_set(){
-    curl -s --create-dirs -o - "$finalurl" | feh --bg-scale -
-}
-
-case "$finalurl" in
-    *fm=jpg*)
-        dl_and_set jpg ;;
-    *fm=png*)
-        dl_and_set png ;;
-esac
+wget -qxO - 'https://source.unsplash.com/random/1920x1080' | feh --bg-scale -
