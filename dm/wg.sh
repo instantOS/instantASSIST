d1b53af7569582846b17895562bcdb4a2587babf
paperbenni <paperbenni@gmail.com>
2019-11-20 18:39:35 +0100

    wg offline capabilities

--- dm/wg.sh
+++ dm/wg.sh
@@ -6,19 +6,22 @@
 
 randomwallpaper() {
     file="$HOME/paperbenni/wallpaper.jpg"
-    url='https://storage.googleapis.com/chromeos-wallpaper-public'
+    if curl google.com; then
+        url='https://storage.googleapis.com/chromeos-wallpaper-public'
 
-    fetch(){
-        IFS='<' read -a array <<< "$(wget -O - -q "$url")"
-        for field in "${array[@]}"; {
-            if [[ "$field" == *_resolution.jpg ]]; then
-                IFS='>' read -a key <<< "$field"
-                printf "%s\n" "${key[1]}"
-            fi
+        fetch() {
+            IFS='<' read -a array <<<"$(wget -O - -q "$url")"
+            for field in "${array[@]}"; do
+                if [[ "$field" == *_resolution.jpg ]]; then
+                    IFS='>' read -a key <<<"$field"
+                    printf "%s\n" "${key[1]}"
+                fi
+            done
         }
-    }
 
-    wget -qO "$file" "$url/$(fetch | shuf -n 1)" && feh --bg-scale "$file"
+        wget -qO "$file" "$url/$(fetch | shuf -n 1)"
+    fi
+    feh --bg-scale "$file"
 }
 
 randomwallpaper
