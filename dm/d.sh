d3bcc56ac1e9af206c438c150005eb4c65ace0d2
paperbenni <paperbenni@gmail.com>
2019-11-26 23:36:32 +0100

    monitor swap script

---
+++ dm/d.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# quickly swap monitors
+
+MONITORS=$(xrandr | grep -Eo 'HDMI-[0-9]*')
+# as of now only supports dual monitor setups
+wc -l <<<"$MONITORS" | grep -q '2' || exit
+
+MONITOR=$(dmenu <<<"$MONITORS")
+[ -n "$MONITOR" ] || exit
+
+OTHERMONITOR=$(grep -v "$MONITOR" <<<"$MONITORS")
+
+xrandr --output "$MONITOR" --left-of "$OTHERMONITOR"
+echo xrandr --output "$MONITOR" --left-of "$OTHERMONITOR" > ~/paperbenni/monitor.sh
--- dm/y.sh
+++ dm/y.sh
@@ -3,13 +3,6 @@
 # download the clipboard link with youtube-dl
 
 LINK="$(xclip -o -selection clipboard)"
-if ! [[ "$LINK" =~ (youtube|soundcloud|vimeo) ]]; then
-    echo "not a supported video link" >&2
-    exit 1
-fi
-
 mkdir -p $HOME/paperbenni/music &>/dev/null
 cd $HOME/paperbenni/music
-echo "ytdl" >~/.status
 youtube-dl -x --audio-format mp3 "$LINK"
-echo "|" >~/.status
