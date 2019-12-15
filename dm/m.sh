0ebbfa339dcb960a30ce80babaa04f1ddc1ce45e
paperbenni <paperbenni@gmail.com>
2019-12-15 15:08:53 +0100

    make symbol paster more robust

--- dm/m.sh
+++ dm/m.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-LETTER="$(cat ~/paperbenni/menus/data/m | dmenu | grep -o '^.')"
+height=$(wc -l ~/paperbenni/menus/data/m)
+
+if [[ $height -gt 30 ]]; then
+    heightfit=30
+else
+    heightfit=$height
+fi
+
+LETTER="$(cat ~/paperbenni/menus/data/m | dmenu -b $heightfit | grep -o '^.')"
+[ -n "$LETTER" ] || exit 0
 
 xdotool type "$LETTER"
