bad99ec8d5b24ed2dbab3aeb119db617056fa3ae
paperbenni <paperbenni@gmail.com>
2019-12-17 18:36:34 +0100

    spotify pause

---
+++ dm/mp.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# pauses spotify player
+pgrep spotify || exit
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
