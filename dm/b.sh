020ddc58a511c0e7a2a14b1ae446632fcf3be95e
paperbenni <paperbenni@gmail.com>
2019-12-26 20:54:48 +0100

    thinkpad e495 support for brightness menu

--- dm/b.sh
+++ dm/b.sh
@@ -1,28 +1,90 @@
 #!/bin/bash
 
+if ! xbacklight &> /dev/null; then
+    [ -e /etc/udev/rules.d/backlight.rules ] || exit
+    BGPU="/sys/class/backlight/"$(cat /etc/udev/rules.d/backlight.rules | head -1 | grep -o 'KERNEL=="[^ ]*"' | grep -o '".*"' | grep -o '[^"]*')"/brightness"
+    MAXBRIGHT=$(cat ${BGPU%/*}/max_brightness)
+    let "BRIGHTSTEP = $MAXBRIGHT / 20"
+fi
+
+brightness() {
+
+    if [ -z "$BGPU" ]; then
+        xbacklight "$@"
+        return
+    fi
+
+    case "$1" in
+    -inc)
+        let "bright = $(cat $BGPU) + $2"
+        if [ "$bright" -lt "$MAXBRIGHT" ] && [ "$bright" -gt 0 ]; then
+            echo "$bright" >"$BGPU"
+        fi
+        ;;
+    -dec)
+        let "bright = $(cat $BGPU) - $2"
+        if [ "$bright" -lt "$MAXBRIGHT" ] && [ "$bright" -gt 0 ]; then
+            echo "$bright" >"$BGPU"
+        fi
+        ;;
+
+    -set)
+        if [ "$2" -lt "$MAXBRIGHT" ] && [ "$2" -gt 0 ]; then
+            echo "$2" >"$BGPU"
+        fi
+        ;;
+
+    *)
+        return
+        ;;
+
+    esac
+
+}
+
+if [ -n "$1" ]; then
+    if grep -q '^[+-]' <<<"$1"; then
+        if [ "${1::1}" = "-" ]; then
+            if grep -q '^[0-9]*$' <<<"${1:1}"; then
+                brightness -dec "${1:1}"
+            fi
+        else
+            if grep -q '^[0-9]*$' <<<"${1:1}"; then
+                brightness -inc "${1:1}"
+            fi
+        fi
+    else
+        if grep -q '^[0-9]*$' <<<"$1"; then
+            brightness -set "$1"
+        fi
+    fi
+    exit
+fi
+
 while :; do
     CHOICE=$(echo '+
 -
 j
 k
 q' | dmenu -n)
+
     if [ "$CHOICE" -eq "$CHOICE" ] &>/dev/null; then
-        xbacklight -set "$CHOICE"
+        brightness -set "$CHOICE"
         break
     fi
 
     case "$CHOICE" in
     +)
-        xbacklight -inc 5
+        brightness -inc "${BRIGHTSTEP:-5}"
         ;;
     -)
-        xbacklight -dec 5
+        brightness -dec "${BRIGHTSTEP:-5}"
         ;;
     j)
-        xbacklight -dec 5
+        brightness -dec "${BRIGHTSTEP:-5}"
         ;;
     k)
-        xbacklight -inc 5
+        brightness -inc "${BRIGHTSTEP:-5}"
         ;;
     *)
         break
