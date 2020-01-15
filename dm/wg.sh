de6fec0d6c46fce682fd38e950506293abc1f9dc
paperbenni <paperbenni@gmail.com>
2020-01-15 16:01:24 +0100

    launch script now works

--- dm/wg.sh
+++ dm/wg.sh
@@ -4,24 +4,4 @@
 ## chromecast like wallpaper changer ##
 #######################################
 
-randomwallpaper() {
-    file="$HOME/paperbenni/wallpaper.jpg"
-    if curl google.com; then
-        url='https://storage.googleapis.com/chromeos-wallpaper-public'
-
-        fetch() {
-            IFS='<' read -a array <<<"$(wget -O - -q "$url")"
-            for field in "${array[@]}"; do
-                if [[ "$field" == *_resolution.jpg ]]; then
-                    IFS='>' read -a key <<<"$field"
-                    printf "%s\n" "${key[1]}"
-                fi
-            done
-        }
-
-        wget -qO "$file" "$url/$(fetch | shuf -n 1)"
-    fi
-    feh --bg-scale "$file"
-}
-
-randomwallpaper
+bash "$HOME/instantos/wallpapers/wall.sh"
