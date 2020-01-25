f0c3408c281dbcab2fa8cef03d445232af68a409
paperbenni <paperbenni@gmail.com>
2020-01-25 18:31:27 +0100

    change y path

--- dm/y.sh
+++ dm/y.sh
@@ -3,6 +3,6 @@
 # download the clipboard link with youtube-dl
 
 LINK="$(xclip -o -selection clipboard)"
-mkdir -p $HOME/paperbenni/music &>/dev/null
-cd $HOME/paperbenni/music
+mkdir -p $(xdg-user-dir MUSIC) &>/dev/null
+cd $(xdg-user-dir MUSIC)
 youtube-dl -x --audio-format mp3 "$LINK"
