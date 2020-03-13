a3d6a6e16878a00c36c77a6396f71f405b37c507
paperbenni <paperbenni@gmail.com>
2020-03-14 00:29:23 +0100

    fix b.sh

--- dm/b.sh
+++ dm/b.sh
@@ -2,101 +2,105 @@
 
 # assist: adjust display brightness
 
-if ! xbacklight &>/dev/null; then
-    [ -e /etc/udev/rules.d/backlight.rules ] || exit
-    BGPU="/sys/class/backlight/"$(head -1 /etc/udev/rules.d/backlight.rules | \
-    grep -o 'KERNEL=="[^ ]*"' | grep -o '".*"' | grep -o '[^"]*')"/brightness"
-    MAXBRIGHT=$(cat ${BGPU%/*}/max_brightness)
-    let "BRIGHTSTEP = $MAXBRIGHT / 20"
+if [ -e /sys/class/backlight/ ] && [ "$(ls /sys/class/backlight | wc -l)" = "1" ]; then
+	BGPU="/sys/class/backlight/$(ls /sys/class/backlight/)"
+	MAXBRIGHT=$(cat "$BGPU/max_brightness")
+	let "BRIGHTSTEP = $MAXBRIGHT / 20"
+else
+	echo "system doesn't support brightness changing or you ran into a bug here"
+	exit 1
 fi
 
 brightness() {
 
-    if [ -z "$BGPU" ]; then
-        xbacklight "$@"
-        return
-    fi
+	BRIGHTNESS="$(cat $BGPU/brightness)"
+	case "$1" in
+	-inc)
+		let "bright = $BRIGHTNESS + $2"
+		echo "$bright"
+		if [ "$bright" -lt "$MAXBRIGHT" ] && [ "$bright" -gt 0 ]; then
+			echo "$bright" >"$BGPU/brightness"
+			echo "$bright"
+		fi
+		;;
+	-dec)
+		let "bright = $BRIGHTNESS - $2"
+		if [ "$bright" -lt "$MAXBRIGHT" ] && [ "$bright" -gt 0 ]; then
+			echo "$bright" >"$BGPU/brightness"
+		fi
+		;;
 
-    case "$1" in
-    -inc)
-        let "bright = $(cat $BGPU) + $2"
-        if [ "$bright" -lt "$MAXBRIGHT" ] && [ "$bright" -gt 0 ]; then
-            echo "$bright" >"$BGPU"
-        fi
-        ;;
-    -dec)
-        let "bright = $(cat $BGPU) - $2"
-        if [ "$bright" -lt "$MAXBRIGHT" ] && [ "$bright" -gt 0 ]; then
-            echo "$bright" >"$BGPU"
-        fi
-        ;;
+	-set)
+		if [ "$2" -lt "$MAXBRIGHT" ] && [ "$2" -gt 0 ]; then
+			echo "$2" >"$BGPU/brightness"
+		fi
+		;;
 
-    -set)
-        if [ "$2" -lt "$MAXBRIGHT" ] && [ "$2" -gt 0 ]; then
-            echo "$2" >"$BGPU"
-        fi
-        ;;
+	*)
+		return
+		;;
 
-    *)
-        return
-        ;;
-
-    esac
+	esac
 
 }
 
+# argument handling before instantmenu
 if [ -n "$1" ]; then
-    if grep -q '^[+-]' <<<"$1"; then
-        if [ "${1::1}" = "-" ]; then
-            if grep -q '^[0-9]*$' <<<"${1:1}"; then
-                brightness -dec "${1:1}"
-            fi
-        else
-            if grep -q '^[0-9]*$' <<<"${1:1}"; then
-                brightness -inc "${1:1}"
-            fi
-        fi
-    else
-        if grep -q '^[0-9]*$' <<<"$1"; then
-            brightness -set "$1"
-        fi
-    fi
-    exit
+	if ! grep -Eq '^(\+|-|[0-9])$' <<<"$1"; then
+		exit
+	fi
+
+	let "BRIGHTSTEP = $BRIGHTSTEP / 4"
+	case "$1" in
+
+	"+")
+		brightness -inc ${BRIGHTSTEP:-5}
+		;;
+	"-")
+
+		brightness -dec ${BRIGHTSTEP:-5}
+		;;
+	*)
+		brightness -set ${2:-50}
+		;;
+
+	esac
+	exit
 fi
 
 while :; do
-    CHOICE=$(echo '+
+	CHOICE=$(echo '+
 -
 j
 k
 q' | instantmenu -n)
 
-    if [ "$CHOICE" -eq "$CHOICE" ] &>/dev/null; then
-        if [ "$CHOICE" = "0" ]; then
-            # 0 doesnt work for some reason
-            brightness -set 1
-        else
-            brightness -set "$CHOICE"
-        fi
-        break
-    fi
+	if [ "$CHOICE" -eq "$CHOICE" ] &>/dev/null; then
+		if [ "$CHOICE" = "0" ]; then
+			# 0 doesnt work for some reason
+			brightness -set 1
+		else
+			brightness -set "$CHOICE"
+		fi
+		break
+	fi
 
-    case "$CHOICE" in
-    +)
-        brightness -inc "${BRIGHTSTEP:-5}"
-        ;;
-    -)
-        brightness -dec "${BRIGHTSTEP:-5}"
-        ;;
-    j)
-        brightness -dec "${BRIGHTSTEP:-5}"
-        ;;
-    k)
-        brightness -inc "${BRIGHTSTEP:-5}"
-        ;;
-    *)
-        break
-        ;;
-    esac
+	case "$CHOICE" in
+	+)
+		brightness -inc "${BRIGHTSTEP:-5}"
+		;;
+	-)
+		brightness -dec "${BRIGHTSTEP:-5}"
+		;;
+	j)
+		brightness -dec "${BRIGHTSTEP:-5}"
+		;;
+	k)
+		brightness -inc "${BRIGHTSTEP:-5}"
+		;;
+	*)
+		break
+		;;
+	esac
 
 done
