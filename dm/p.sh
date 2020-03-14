118a397a19a149aee1203f99b04823ed2fef2437
paperbenni <paperbenni@gmail.com>
2020-03-14 15:53:20 +0100

    p.sh sink cache

--- dm/p.sh
+++ dm/p.sh
@@ -2,13 +2,27 @@
 
 # assist: adjust audio volume
 
-PSINK=$(pactl list sinks | pcregrep -iM ".*(sink|ziel).*\n.*running" | head -1 | grep -o '[0-9]*')
+if ! [ -e /tmp/instantos/pasink ]
+then
+	mkdir /tmp/instantos
+	if [ "$(pactl list sinks | grep -Ei '(sink|ziel)' | wc -l)" = 1 ]
+	then
+		echo "single sink detected"
+		pactl list sinks | grep -Ei '(sink|ziel)' | grep -o '[0-9]*' > /tmp/instantos/pasink
+	else
+		echo "attempting multi sink detection"
+		pactl list sinks | pcregrep -iM ".*(sink|ziel).*\n.*running" | head -1 | grep -o '[0-9]*' > /tmp/instantos/pasink
+	fi
+fi
+
+PSINK="$(cat /tmp/instantos/pasink | grep -o '[0-9]*')"
 
 pvolume() {
-    pactl set-sink-volume $PSINK "$1"
+    pactl set-sink-volume "$PSINK" "$1"
 }
 
 while :; do
+	# either instantmenu or args
     if [ -n "$1" ]; then
         CHOICE="$1"
     else
@@ -18,6 +32,7 @@ j
 k
 m
 q' | instantmenu -n)
+
     fi
 
     if [ "$CHOICE" -eq "$CHOICE" ] &>/dev/null; then
