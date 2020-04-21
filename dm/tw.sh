8c352e00e76a37b125f0d42fc359d51c719a896f
paperbenni <paperbenni@gmail.com>
2020-04-21 20:14:09 +0200

    fix selectwm

--- dm/tw.sh
+++ dm/tw.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-WMSELECT"$(cat /opt/instantos/menus/data/wm | instantmenu -c -p -l 10 'select window manager')"
+WMSELECT="$(cat /opt/instantos/menus/data/wm | instantmenu -c -l 10 -p 'select window manager')"
+
+if [ -z "$WMSELECT" ]; then
+    exit
+fi
 
 if command -v "$WMSELECT" &>/dev/null; then
+    echo "setting the wm to $WMSELECT"
     echo "$WMSELECT" >/tmp/wmoverride
     sleep 0.2
     killwm
