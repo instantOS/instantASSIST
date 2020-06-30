e27261bb70a9ee3ad4f24f5d14cd5074fca4f59d
paperbenni <paperbenni@gmail.com>
2020-06-30 21:08:03 +0200

    add st

---
+++ dm/st.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# assist: take a screenshot of a selected area after 5 seconds
+
+slop=$(slop -f "%g") || exit 1
+read -r G < <(echo "$slop")
+sleep 5
+import -window root -crop "$G" $(xdg-user-dir PICTURES)/$(date '+%Y%m%d%H%M%S').png
