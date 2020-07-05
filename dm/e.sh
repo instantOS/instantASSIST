dac7ab1ad24420fa980746ae8efb2752a42304b4
paperbenni <paperbenni@gmail.com>
2020-07-05 14:12:39 +0200

    add emoji keyboard

--- dm/e.sh
+++ dm/e.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
+# assist: emoji picker
 
-# assist: type emojis or special symbols
+cd
 
-height=$(wc -l < /opt/instantos/menus/data/m)
+# exit and remove leftover files
+fail() {
+    [ -e ~/.cache/emoji/list.txt ] && rm ~/.cache/emoji/list.txt
+    if [ -n "$1" ]; then
+        notify-send "error: $1"
+    else
+        notify-send 'emoji download failed'
+    fi
+    exit 1
+}
 
-if [[ "$height" -gt 30 ]]; then
-    heightfit=30
-else
-    heightfit="$height"
+# fetch list of emojis from unicode.org
+if ! [ -e .cache/emoji/list.txt ]; then
+    notify-send "downloading emoji list"
+    mkdir -p .cache/emoji
+    cd .cache/emoji
+
+    if ! checkinternet && ! ping -c 1 google.com; then
+        fail "no internet"
+    fi
+
+    if ! curl -s 'https://unicode.org/Public/emoji/13.0/emoji-test.txt' |
+        grep 'E[0-9]' | grep '#' | grep -o '#.*' |
+        sed 's/# \(.*\)E[0-9]*\.[0-9]* \(.*\)/\2 \1/g' >list.txt; then
+        fail "unknown error"
+    fi
+
+    if ! [ "$(wc -l list.txt | grep -o '^[0-9]*')" -gt 1000 ]; then
+        fail "list too short"
+    fi
+
 fi
 
-LETTER=$(cat /opt/instantos/menus/data/m | instantmenu -b -l "$heightfit" | grep -o '^.')
-[ -n "$LETTER" ] || exit 0
+# pick emojis using 
+cd
 
-xdotool type "$LETTER"
+EMOJI="$(cat .cache/emoji/list.txt | instantmenu -l 20 -p 'emoji picker' | grep -o ' [^a-zA-Z0-9]*$' | grep -o '[^ ]*')"
+if [ -z "$EMOJI" ]; then
+    exit
+fi
+
+xdotool type "$EMOJI"
