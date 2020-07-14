fedef3c241f76e76f29c6b291741453a26ca1ba6
paperbenni <paperbenni@gmail.com>
2020-07-14 19:46:01 +0200

    rename volume slider and add sent

---
+++ dm/a.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/dash
+# assist: audio volume slider
+
+CURVOL="$(amixer get Master | grep -Eo '[0-9]{1,3}%' | head -1 | grep -o '[0-9]*')"
+islide -s "$CURVOL" -c "/opt/instantos/menus/ex/p.sh "
