4a52e22ebd9c510eb3b78c57e495d5747c32dd65
paperbenni <paperbenni@gmail.com>
2020-07-20 16:56:27 +0200

    redo pulse settings

--- ex/p.sh
+++ ex/p.sh
@@ -2,68 +2,31 @@
 
 # assist: adjust audio volume
 
-if ! [ -e /tmp/instantos/pasink ]
-then
-	mkdir /tmp/instantos
-	if [ "$(pactl list sinks | grep -Ei '(sink|ziel)' | wc -l)" = 1 ]
-	then
-		echo "single sink detected"
-		pactl list sinks | grep -Ei '(sink|ziel)' | grep -o '[0-9]*' > /tmp/instantos/pasink
-	else
-		echo "attempting multi sink detection"
-		pactl list sinks | pcregrep -iM ".*(sink|ziel).*\n.*running" | head -1 | grep -o '[0-9]*' > /tmp/instantos/pasink
-	fi
-fi
-
-PSINK="$(cat /tmp/instantos/pasink | grep -o '[0-9]*')"
-
 pvolume() {
-    pactl set-sink-volume "$PSINK" "$1"
+    pactl amixer set 'Master' "$1"
 }
 
-while :; do
-	# either instantmenu or args
-    if [ -n "$1" ]; then
-        CHOICE="$1"
-    else
-        CHOICE=$(echo '+
--
-j
-k
-m
-q' | instantmenu -n)
+if [ "$1" -eq "$1" ] &>/dev/null; then
+    pvolume "$CHOICE"
+fi
 
-    fi
-
-    if [ "$CHOICE" -eq "$CHOICE" ] &>/dev/null; then
-        pvolume "$CHOICE"%
-        break
-    fi
-
-    case "$CHOICE" in
-    +)
-        pvolume +5%
-        ;;
-    -)
-        pvolume -5%
-        ;;
-    j)
-        pvolume -5%
-        ;;
-    k)
-        pvolume +5%
-        ;;
-    m)
-        if ! pactl set-sink-mute 0 toggle; then
-            pactl set-sink-mute $PSINK toggle
-
-        fi
-        break
-        ;;
-    *)
-        break
-        ;;
-    esac
-
-    [ -n "$1" ] && break
-done
+case "$1" in
++)
+    pvolume 5%+
+    ;;
+-)
+    pvolume 5%-
+    ;;
+j)
+    pvolume 5%-
+    ;;
+k)
+    pvolume 5%+
+    ;;
+m)
+    amixer set 'Master' toggle
+    ;;
+*)
+    echo "not setting audio"
+    ;;
+esac
