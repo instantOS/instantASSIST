efebb51e8ba0ed52c83a91f3f11c3b3896cf96fb
con-f-use <con-f-use@users.noreply.github.com>
2020-07-21 00:44:20 +0200

    fix stuck clipboard

--- dm/sc.sh
+++ dm/sc.sh
@@ -2,6 +2,7 @@
 
 # assist: take a screenshot of a selected area into the clipboard
 
-slop=$(slop -f "%g") || exit 1
+slop=$(slop -f "%g") || 
+    exit 1
 import -window root -crop "$slop" png:- |
-    xclip -selection clipboard -t image/png
+    xclip -selection clipboard -t image/png &>/dev/null
