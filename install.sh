ac8a85eea5d72d794848cf00ec5b059708a7c278
paperbenni <paperbenni@gmail.com>
2020-07-21 16:31:40 +0200

    rename folder

--- install.sh
+++ install.sh
@@ -4,32 +4,19 @@
 ## install instantASSIST ##
 ###########################
 
-# please run as root
-if [ -z "$ASSISTPREFIX" ]; then
-    if ! [ "$(whoami)" = "root" ]; then
-        echo "please run as root"
-        exit 1
-    fi
-else
-    mkdir -p "${ASSISTPREFIX}"/usr/bin
-    mkdir -p "${ASSISTPREFIX}"/opt
+git pull
+
+ASSISTPREFIX="${ASSISTPREFIX:-/}"
+
+[ -e "${ASSISTPREFIX}"/usr/bin ] || mkdir -p "${ASSISTPREFIX}"/usr/bin
+[ -e "${ASSISTPREFIX}"/usr/share ] || mkdir -p "${ASSISTPREFIX}"/usr/share
+
+# remove old version
+if [ -e "${ASSISTPREFIX}"/usr/share/instantassist ]; then
+    rm -rf "${ASSISTPREFIX}"/usr/share/instantassist
 fi
 
-source /usr/share/paperbash/import.sh
-pb install
-pb git
-
-echo "installing instantASSIST"
-dom='https://github.com'
-
-if [ -e "${ASSISTPREFIX}"/usr/share/instantassist/menus/dm ]; then
-    rm -rf "${ASSISTPREFIX}"/usr/share/instantassist/menus/dm
-fi
-
-rm -rf "${ASSISTPREFIX}"/usr/share/instantassist/menus
-mkdir -p "${ASSISTPREFIX}"/usr/share/instantassist/menus &>/dev/null
-mkdir /tmp/instantmenus &>/dev/null
-cd /tmp/instantmenus
+bash cache.sh
 
 if ! [ -e "${ASSISTPREFIX}"/usr/share/instantassist/spotify-adblock.so ]; then
     git clone --depth=1 "$dom/abba23/spotify-adblock-linux.git"
@@ -41,12 +28,8 @@ if ! [ -e "${ASSISTPREFIX}"/usr/share/instantassist/spotify-adblock.so ]; then
     rm -rf spotify-adblock-linux
 fi
 
-rm -rf instantASSIST
-gitclone instantOS/instantASSIST
-cd instantASSIST
-
 instusrbin() {
-    chmod +x "$1"
+    chmod 755 "$1"
     mv "$1" "${ASSISTPREFIX}"/usr/bin/"$1"
 }
 
