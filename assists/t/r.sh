7a1b42f87f3164a993bcd36f210fccb1baed330e
paperbenni <paperbenni@gmail.com>
2020-07-22 09:50:46 +0200

    add redshift

---
+++ assists/t/r.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# assist: set redshift brightness
+
+BRIGHTNESS=$(echo '' | instantmenu -p 'set redshift brightness')
+[ -z "$BRIGHTNESS" ] && exit
+
+instantinstall redshift
+if grep -q '^[0-9]*\.[0-9]*$' <<<"$BRIGHTNESS"; then
+    pgrep redshift && pkill redshift
+    redshift -b "$BRIGHTNESS"
+elif grep -q '^1$' <<<"$BRIGHTNESS"; then
+    pkill redshift
+else
+    notify-send "enter a value between 0 and 1"
+fi
