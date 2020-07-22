01cd3be1d0f72f953aed5f82f1bed976a948a884
paperbenni <paperbenni@gmail.com>
2020-07-23 00:33:41 +0200

    fix play/pause

--- assists/m.sh
+++ assists/m.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
-# assist: spotifyctl
+# assist: play/pause music
 
-command -v spotify &>/dev/null || {
+if ! command -v spotify &>/dev/null; then
     notify-send '[instantASSIST] please install spotify first'
     exit 0
-}
+else
+    playerctl play-pause
+    exit
+fi
 
 if pgrep spotify &>/dev/null; then
     # pauses spotify player
     dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
     exit
 else
-    if timeout 4 playerctl status; then
+    if ! playerctl status || true | grep .; then
         playerctl play-pause
     else
         LD_PRELOAD="/usr/share/instantassist/spotify-adblock.so" spotify
