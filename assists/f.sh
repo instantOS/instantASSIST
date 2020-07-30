522df23fa127ba22c34f84fb8f23e43bb7b81a3c
paperbenni <paperbenni@gmail.com>
2020-07-30 13:25:46 +0200

    allow customizing browser list

--- assists/f.sh
+++ assists/f.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/dash
 
 # assist: web browser
+LIST="$(cat /usr/share/instantassist/data/firefox)"
+if [ -e ~/.config/instantos/instantassist/browser ]; then
+    LIST="$LIST
+$(cat ~/.config/instantos/instantassist/browser)"
+fi
 
-LINK="$(instantmenu -ct </usr/share/instantassist/data/firefox)"
+LINK="$(echo "$LIST" | instantmenu -ct | sed 's/^.//g')"
 
 [ -z "$LINK" ] && exit
 
