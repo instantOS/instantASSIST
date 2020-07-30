6c6446155bd4a00624cfd783c588c2b3ec8bab74
paperbenni <paperbenni@gmail.com>
2020-07-30 13:16:51 +0200

    rework firefox instantassist

--- assists/f.sh
+++ assists/f.sh
@@ -1,53 +1,22 @@
 #!/usr/bin/dash
 
-# assist: firefox
+# assist: web browser
 
-if ! [ -e ~/instantos/data/firefox ]; then
-    echo "building firefox cache"
-    mkdir -p ~/instantos/data
-    cat /usr/share/instantassist/assists/f.sh | grep '^[a-z])' | grep -o '[a-z]' >~/instantos/data/firefox
-fi
+LINK="$(instantmenu -ct </usr/share/instantassist/data/firefox)"
 
-LINK=$(instantmenu -p "firefox" -n <~/instantos/data/firefox)
+[ -z "$LINK" ] && exit
 
 case "$LINK" in
-s)
-    firefox github.com
-    ;;
-y)
-    firefox youtube.com
-    ;;
-p)
-    firefox paperbenni.github.io
-    ;;
-i)
-    firefox instantos.github.io
-    ;;
-h)
-    firefox github.com
-    ;;
-d)
-    firefox dsbmobile.de
-    ;;
-g)
-    firefox google.com
-    ;;
-r)
-    firefox reddit.com
-    ;;
-c)
+clipboard)
     firefox "$(xclip -o)"
     ;;
-t)
-    firefox trello.com
-    ;;
-w)
+*firefox)
     firefox --private-window
     ;;
-f)
+*tab)
     firefox
     ;;
 *)
-    firefox
+    firefox "$LINK"
     ;;
 esac
