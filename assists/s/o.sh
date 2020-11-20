518c2d625f7724810810c5bba820b7bec0bf6890
paperbenni <paperbenni@gmail.com>
2020-11-20 16:19:01 +0100

    chmod and better s/o assist

--- assists/s/o.sh
+++ assists/s/o.sh
@@ -8,5 +8,14 @@ SCDIR="/tmp/instantos/screenshot/$(whoami)"
 
 mkdir -p "$SCDIR"
 
-import -window root -crop "$G" "$SCDIR"/tmp.png
-feh "$SCDIR"/tmp.png
+PICNAME="$(date "+%s")".png
+
+import -window root -crop "$G" "$SCDIR"/"$PICNAME"
+
+# make feh floating
+instantwmctrl specialnext 1
+feh --geometry "$G" "$SCDIR"/"$PICNAME" &
+sleep 1
+
+rm "$SCDIR"/"$PICNAME"
+
