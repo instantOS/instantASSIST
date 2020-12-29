c147a2b73ccdb6ddbebf5cf25daed45f6dabc803
bob8677 <68208828+bob8677@users.noreply.github.com>
2020-12-29 16:00:18 +0000

    add -n option to echo in window rename assist

--- assists/n/n.sh
+++ assists/n/n.sh
@@ -2,7 +2,7 @@
 
 # assist: rename current window
 
-NEWNAME="$(echo '' | instantmenu -h -1 -p 'rename window' -c -bw 4)"
+NEWNAME="$(echo -n '' | instantmenu -h -1 -p 'rename window' -c -bw 4)"
 [ -z "$NEWNAME" ] && exit
 sleep 0.2
 xdotool getactivewindow set_window --name "$NEWNAME"
