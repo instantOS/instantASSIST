53ef5ca8321edb240aca6b5c3ca8c57f25da352e
Dušan Uverić <dusan.uveric9@gmail.com>
2021-02-09 21:00:29 +0100

    read microphone status from pulse device
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- assists/v/m.sh
+++ assists/v/m.sh
@@ -4,7 +4,7 @@
 
 amixer -D pulse sset Capture toggle
 
-MICSTATUS=$(amixer get Capture | grep -w -o -e "on\|off" | sort -u | tr "[a-z]" "[A-Z]")
+MICSTATUS=$(amixer -D pulse get Capture | grep -w -o -e "on\|off" | sort -u | tr "[a-z]" "[A-Z]")
 
 if [ $MICSTATUS == "OFF" ]; then
     MICICON="/usr/share/icons/Papirus-Dark/symbolic/status/microphone-sensitivity-muted-symbolic.svg"
