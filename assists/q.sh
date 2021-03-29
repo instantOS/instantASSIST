1f9843f628298e709a375d4ecd9c795fae6eddd4
paperbenni <paperbenni@gmail.com>
2021-03-29 21:19:10 +0200

    add tuxi assist

---
+++ assists/q.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+[ -e /usr/bin/tuxi ] || instantinstall tuxi-git
+
+# assist: ask tuxi a question
+
+# TODO (maybe?) add search history/history of outputs
+
+QUESTION="$(echo "" | instantmenu -p 'ask tuxi a question')"
+[ -z "$QUESTION" ] && exit
+
+st -e "dash" -c "tuxi '$QUESTION' | less -r --mouse --wheel-lines 3"
+
