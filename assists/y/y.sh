6588d1ff1417367d59ce4c50297040755c78c1b5
TomixUG <tomas.sousek1@gmail.com>
2021-04-18 00:28:28 +0200

    Error, success notification (youtube-dl)

--- assists/y/y.sh
+++ assists/y/y.sh
@@ -14,4 +14,5 @@ fi
 mkdir -p "$(xdg-user-dir MUSIC)" &>/dev/null
 cd "$(xdg-user-dir MUSIC)" || exit 1
 notify-send -a instantASSIST ' downloading audio'
-youtube-dl --format bestaudio --add-metadata --playlist-items 1 -x "$LINK" || exit
+youtube-dl --format bestaudio --add-metadata --playlist-items 1 -x "$LINK" || { notify-send -a instantASSIST " audio download failed"; exit 1;}
+notify-send -a instantASSIST " audio downloaded ($(xdg-user-dir MUSIC))";
