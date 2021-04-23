6048e9360caf4755c80bfbe179c7b51dc23ae24f
paperbenni <paperbenni@gmail.com>
2021-04-23 12:27:41 +0200

    rework filepicker

--- assists/o.sh
+++ assists/o.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # assist: open file in text editor
 
-st -c instantfloat -e bash -c /usr/share/instantassist/utils/o.sh
+CHOICE="$(instantfilepick)" || exit
+
+instantutils open editor "$CHOICE"
