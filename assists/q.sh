3fcb482ee4ebce7251c0a24e2b0d26be98a4c2bc
Pebor <peborisbest@gmail.com>
2021-05-02 11:31:04 +0200

    make tuxi more integrated

--- assists/q.sh
+++ assists/q.sh
@@ -6,8 +6,29 @@
 
 # TODO (maybe?) add search history/history of outputs
 {
-QUESTION="$(echo "" | instantmenu -p 'ask tuxi a question')"
-[ -z "$QUESTION" ] && exit
+NEWQUESTION=true
+while [ -n "$NEWQUESTION" ]; do
 
-st -e "dash" -c "tuxi '$QUESTION' | less -r --mouse --wheel-lines 3"
+    unset NEWQUESTION
+    QUESTION="$(echo "" | instantmenu -p 'ask tuxi a question')"
+    [ -z "$QUESTION" ] && exit
+
+    ANWSER="$(tuxi -r -q "$QUESTION")"
+
+    CHOICE="$(echo "$(sed "s/^/>>h/" <<< $ANWSER)
+:b New question
+:b Copy to clipboard
+:r Close" | instantmenu -h -1 -l 20 -q Tuxi -ps $(wc -l <<< $ANWSER))"
+
+    case "$CHOICE" in
+    *question)
+        NEWQUESTION=true
+        ;;
+    *clipboard)
+        echo "$ANWSER" | xclip -selection c
+        notify-send "copied anwser to clipboard"
+        ;;
+    esac
+
+done
 }
