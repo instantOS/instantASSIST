1d228e7656f3dffac73284e2ff6433cc0bcffe9d
Dušan Uverić <dusan.uveric9@gmail.com>
2021-05-06 15:12:02 +0200

    add assist for renaming tags
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

---
+++ assists/n/t.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/dash
+
+# assist: rename current tag
+
+NEWNAME="$(imenu -i 'rename tag (15 char max length)')"
+[ -z "$NEWNAME" ] && exit
+[ ${#NEWNAME} -gt 15 ] && /usr/share/instantassist/assists/n/t.sh && exit
+instantwmctrl nametag "$NEWNAME"
+
