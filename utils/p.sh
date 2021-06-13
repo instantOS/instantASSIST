ac107bd6450e6f66891e32ee15bfc7b172e674d4
paperbenni <paperbenni@gmail.com>
2021-06-13 23:15:12 +0200

    add dunst volume indicator

--- utils/p.sh
+++ utils/p.sh
@@ -7,35 +7,49 @@ pvolume() {
 }
 
 if [ "$1" -eq "$1" ] &>/dev/null; then
-    pvolume "${1}%"
+    pamixer --set-volume "$1" || pvolume "${1}%"
     exit
 fi
 
+displayvolume() {
+    VOLUME="$(pamixer --get-volume || {
+        awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master) | grep -o '[0-9]*'
+    })"
+    dunstify -i -a instantASSIST '/usr/share/icons/Papirus-Dark/symbolic/status/audio-volume-medium-symbolic.svg' -h int:value:"$VOLUME" Volume
+}
+
 case "$1" in
 +)
-    pvolume 5%+
+    pamixer -i 5 || pvolume 5%+
+    displayvolume
     ;;
 -)
-    pvolume 5%-
+    pamixer -d 5 || pvolume 5%-
+    displayvolume
     ;;
 j)
-    pvolume 5%-
+    pamixer -d 5 || pvolume 5%-
+    displayvolume
     ;;
 k)
-    pvolume 5%+
+    pamixer -i 5 || pvolume 5%+
+    displayvolume
     ;;
 m)
-    if iconf -i mute
-    then
+    if iconf -i mute; then
         iconf -i mute 0
-        amixer set Master unmute || amixer -D pulse set Master unmute
-        amixer set Headphone unmute || amixer -D pulse set Headphone unmute
-        amixer set Speaker unmute || amixer -D pulse set Speaker unmute
+        pamixer -u || {
+            amixer set Master unmute || amixer -D pulse set Master unmute
+            amixer set Headphone unmute || amixer -D pulse set Headphone unmute
+            amixer set Speaker unmute || amixer -D pulse set Speaker unmute
+        }
     else
         iconf -i mute 1
-        amixer set Master mute || amixer -D pulse set Master mute
-        amixer set Headphone mute || amixer -D pulse set Headphone mute
-        amixer set Speaker mute || amixer -D pulse set Speaker mute
+        pamixer -m || {
+            amixer set Master mute || amixer -D pulse set Master mute
+            amixer set Headphone mute || amixer -D pulse set Headphone mute
+            amixer set Speaker mute || amixer -D pulse set Speaker mute
+        }
     fi
     ;;
 *)
