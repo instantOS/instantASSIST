9195daf8fc26ad17eb794b3737270e70606cd24e
paperbenni <paperbenni@gmail.com>
2021-07-03 10:03:44 +0200

    add ocr assist

---
+++ assists/s/r.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# assist: detect text from selected area
+
+# inspired by https://old.reddit.com/r/commandline/comments/oceuu3/nifty_little_ocr_script_which_i_use_a_lot_maybe/
+
+instantinstall tesseract tesseract-data-eng maim notify-send xclip || exit 1
+
+IMAGE_FILE="$(xdg-user-dir PICTURES)"/ocr.png
+G=$(instantslop -f "%g") || exit 1
+import -window root -crop "$G" "$IMAGE_FILE" || exit 1
+
+DETECTEDTEXT="$(tesseract "$IMAGE_FILE" stdout | sed 's/\x0c//')"
+
+if [ -z "$DETECTEDTEXT" ]; then
+    notify-send -a instantASSIST "no text detected"
+    exit 1
+fi
+
+xclip -selection clip <<<"$DETECTEDTEXT"
+notify-send -a instantASSIST "Detected text
+$DETECTEDTEXT"
