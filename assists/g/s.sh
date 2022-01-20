3ea0ecea4993064d89d24b86275531c695d41be9
paperbenni <paperbenni@gmail.com>
2022-01-20 22:51:24 +0100

    fix ssh scanner assist

--- assists/g/s.sh
+++ assists/g/s.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# assist: ssh scanner
+# assist: scan local network for ssh servers
 
 instantinstall nmap
+instantinstall ifconfig
+
 getlocalip() {
-    if ! command -v ifconfig; then
+    if ! command -v ifconfig &>/dev/null; then
         instantinstall net-tools || exit 1
     fi
+
     TEMPIP="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
+
     if grep -q '192' <<<"$TEMPIP"; then
         echo "$TEMPIP" | grep '192' | tail -1
     else
@@ -39,6 +43,7 @@ fi
 # ensure server list is not empty
 if ! grep -q ... ~/.cache/instantos/localssh; then
     if imenu -c "no ssh servers found in the local network, rescan?"; then
+        scanservers
         while ! grep -q ... ~/.cache/instantos/localssh; do
             if imenu -c "no ssh servers found in the local network, rescan?"; then
                 scanservers
@@ -55,10 +60,16 @@ processchoice() {
         {
             echo "rescan servers"
             cat ~/.cache/instantos/localssh
+            echo ":rclose"
         } | imenu -l "ssh server browser"
     )"
 
     [ -z "$CHOICE" ] && exit
+
+    if grep -q ':rclose' <<<"$CHOICE"; then
+        echo 'no server selected, quitting'
+        exit
+    fi
 
     if grep -q 'rescan' <<<"$CHOICE"; then
         scanservers
