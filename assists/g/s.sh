ac5ce2c0e4cc8cf96b1952d4980e64369c76bd22
paperbenni <paperbenni@gmail.com>
2023-09-16 19:04:06 +0200

    make linter happy

--- assists/g/s.sh
+++ assists/g/s.sh
@@ -6,7 +6,7 @@ instantinstall nmap || exit 1
 instantinstall net-tools || exit 1
 
 iface="$(ip a | grep -oP '2: \K\w*')"
-local_ip="$(ip -br address show primary $iface | grep -oE '((25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])')"
+local_ip="$(ip -br address show primary "$iface" | grep -oE '((25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])')"
 
 getlocalip() {
     TEMPIP="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
