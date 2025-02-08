145cc4925045f34dc47cf20dd3799d8c46056782
paperbenni <paperbenni@gmail.com>
2025-02-08 20:37:39 +0100

    add wayland warning

--- assists/c.sh
+++ assists/c.sh
@@ -2,6 +2,11 @@
 
 # assist: toggle compositing
 
+if [ -z "$XDG_SESSION_TYPE" = "wayland" ]; then
+    notify-send -a instantASSIST ' you on wayland you doofus'
+    exit
+fi
+
 if pgrep picom; then
     notify-send -a instantASSIST ' compositing disabled'
     pkill picom
