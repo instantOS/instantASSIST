9ce769bc7fc45ffd9c3c6f360d21781f881ef955
paperbenni <paperbenni@gmail.com>
2025-05-16 10:43:33 +0200

    more flameshot wayland corrections

--- assists/s/a.sh
+++ assists/s/a.sh
@@ -4,14 +4,19 @@
 # TODO: configure flameshot for wayland
 
 instantinstall flameshot
-sleep 0.1
 if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
     export SDL_VIDEODRIVER=wayland
     export _JAVA_AWT_WM_NONREPARENTING=1
     export QT_QPA_PLATFORM=wayland
     export XDG_CURRENT_DESKTOP=sway
     export XDG_SESSION_DESKTOP=sway
+    if ! pgrep flameshot > /dev/null; then
+        flameshot &
+        sleep 2
+    fi
     flameshot gui
 else
+    # TODO: why is the sleep needed?
+    sleep 0.1
     flameshot gui
 fi
