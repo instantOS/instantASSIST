d414ea2b2b1d20c5b53eaae37c9d897ac272addb
paperbenni <paperbenni@gmail.com>
2025-07-14 14:42:07 +0200

    make screen freeze support sway

--- assists/s/o.sh
+++ assists/s/o.sh
@@ -2,20 +2,37 @@
 
 # assist: "freeze" part of the screen
 
-G=$(instantslop -f "%g") || exit 1
-
-SCDIR="/tmp/instantos/screenshot/$(whoami)"
-
-mkdir -p "$SCDIR"
-
+SCDIR="$(mktemp -d)"
 PICNAME="$(date "+%s")".png
 
-import -window root -crop "$G" "$SCDIR"/"$PICNAME"
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    G=$(instantslop) || exit 1
+    grim -g "$G" "$SCDIR/$PICNAME"
+else
+    G=$(instantslop -f "%g") || exit 1
+    import -window root -crop "$G" "$SCDIR/$PICNAME"
+fi
 
 # make feh floating
-instantwmctrl specialnext 1
-feh --title "instantASSIST screen freeze" --geometry "$G" "$SCDIR"/"$PICNAME" &
-sleep 1
+if [ "$XDG_SESSION_TYPE" = "wayland" ] && command -v swaymsg >/dev/null && [ -n "$SWAYSOCK" ]; then
+    # Parse geometry: G="X,Y WIDTHxHEIGHT"
+    X=$(echo "$G" | awk -F'[ ,x]' '{print $1}')
+    Y=$(echo "$G" | awk -F'[ ,x]' '{print $2}')
+    W=$(echo "$G" | awk -F'[ ,x]' '{print $3}')
+    H=$(echo "$G" | awk -F'[ ,x]' '{print $4}')
+    feh --title "instantASSIST screen freeze" --geometry "$G" "$SCDIR/$PICNAME" &
+    FEH_PID=$!
+    # Wait for feh to appear in the tree
+    for i in {1..20}; do
+        sleep 0.1
+        swaymsg '[title="instantASSIST screen freeze"]' floating enable && \
+        swaymsg "[title=\"instantASSIST screen freeze\"]" move position "$X" "$Y" && \
+        swaymsg "[title=\"instantASSIST screen freeze\"]" resize set "$W" px "$H" px && break
+    done
+else
+    instantwmctrl specialnext 1
+    feh --title "instantASSIST screen freeze" --geometry "$G" "$SCDIR/$PICNAME" &
+fi
 
-rm "$SCDIR"/"$PICNAME"
-
+sleep 5
+rm -rf "$SCDIR"
