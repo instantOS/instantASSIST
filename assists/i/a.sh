af6d5999cca5a6ff0558bc097c04569de304f100
paperbenni <paperbenni@gmail.com>
2025-07-14 15:39:58 +0200

    add gemini and claude assists

---
+++ assists/i/a.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# assist: ask Anthropic about the clipboard content
+
+/usr/share/instantassist/utils/i.sh
---
+++ assists/i/g.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# assist: ask Gemini about the clipboard content
+
+/usr/share/instantassist/utils/i.sh
